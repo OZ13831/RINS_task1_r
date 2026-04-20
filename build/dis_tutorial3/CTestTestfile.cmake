27e9dfd48fc8e4e06b4875ca9ba9723eac3f5365
OZ13831 <oz13831@student.uni-lj.si>
2026-04-20 10:04:29 +0200

    Vse dodano

---
+++ build/dis_tutorial3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gamma/colcon_ws/src/dis_tutorial3
+# Build directory: /home/gamma/colcon_ws/build/dis_tutorial3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
